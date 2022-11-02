5f47cc0e6fc2fea2302171e752ef381421be031d
Galymzhan <gaayapbergen@gmail.com>
2022-11-02 15:06:16 +0500

    My Calorie app, first commit

---
+++ blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Personal(models.Model):
+    username = models.CharField(max_length=50)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=20)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.username
+
+
+class Data(models.Model):
+    person = models.ForeignKey(Personal, on_delete=models.CASCADE)
+    height = models.IntegerField(default=0)
+    weight = models.IntegerField(default=0)
+    comments = models.TextField()
+    date = models.DateTimeField('date')
+
+    def __str__(self):
+        return self.person
